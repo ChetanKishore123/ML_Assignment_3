cb06f80493136e8d3ca0e411b3741b447dfbd650
ChetanKishore123 <88477154+ChetanKishore123@users.noreply.github.com>
2023-04-27 16:58:15 +0530

    Update c.py

--- c.py
+++ c.py
@@ -4,61 +4,49 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, roc_auc_score
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Function to generate dataset and calculate ROC AUC
-def generate_dataset_and_roc_auc(weight):
-    # generate 2 class dataset
-    X, y = make_classification(n_samples=1000, n_classes=2, weights=[weight, 1 - weight], random_state=1)
+np.random.seed(1234)
+
+st.title("Hierarchical Clustering")
+
+st.write("Create a slider for each step in hierarchical clustering of unsupervised data. Use the sklearn.datasets make_blobs function to create the dataset and visualize the predictions using a changing voronoi diagram.")
+
+weight = st.sidebar.slider("imbalance ratio", min_value=0.1, max_value=0.9)
+
+X, y = make_classification(n_samples=1000, n_classes=2, weights=[weight, 1 - weight], random_state=1)
     # split into train/test sets with same class ratio
-    trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2, stratify=y)
-    # no skill model, stratified random class predictions
-    model = DummyClassifier(strategy='stratified')
-    model.fit(trainX, trainy)
-    yhat = model.predict_proba(testX)
-    naive_probs = yhat[:, 1]
-    # calculate roc auc
-    roc_auc_naive = roc_auc_score(testy, naive_probs)
-    # skilled model
-    model = LogisticRegression(solver='lbfgs')
-    model.fit(trainX, trainy)
-    yhat = model.predict_proba(testX)
-    model_probs = yhat[:, 1]
-    # calculate roc auc
-    roc_auc_model = roc_auc_score(testy, model_probs)
-    # plot roc curves
-    plot_roc_curve(testy, naive_probs, model_probs)
-    # print ROC AUC values
-    print('No Skill ROC AUC: %.3f' % roc_auc_naive)
-    print('Logistic ROC AUC: %.3f' % roc_auc_model)
-    print('Weight', weight)
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2, stratify=y)
+# no skill model, stratified random class predictions
+model = DummyClassifier(strategy='stratified')
+model.fit(trainX, trainy)
+yhat = model.predict_proba(testX)
+naive_probs = yhat[:, 1]
+# calculate roc auc
+roc_auc_naive = roc_auc_score(testy, naive_probs)
+# skilled model
+model = LogisticRegression(solver='lbfgs')
+model.fit(trainX, trainy)
+yhat = model.predict_proba(testX)
+model_probs = yhat[:, 1]
 
-# plot no skill and model roc curves
-def plot_roc_curve(test_y, naive_probs, model_probs):
-    # plot naive skill roc curve
-    fpr, tpr, _ = roc_curve(test_y, naive_probs)
-    plt.plot(fpr, tpr, linestyle='--', label='No Skill')
-    # plot model roc curve
-    fpr, tpr, _ = roc_curve(test_y, model_probs)
-    plt.plot(fpr, tpr, marker='.', label='Logistic')
-    # axis labels
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    # show the legend
-    plt.legend()
-    # show the plot
-    plt.show()
+roc_auc_model = roc_auc_score(testy, model_probs)
 
+fpr, tpr, _ = roc_curve(testy, naive_probs)
 
+plt.plot(fpr, tpr, linestyle='--', label='No Skill')
+# plot model roc curve
+fpr, tpr, _ = roc_curve(testy, model_probs)
+plt.plot(fpr, tpr, marker='.', label='Logistic')
+# axis labels
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+
+# plt.figure()
+# ax = plt.Subplot(fig, 111)
 fig = plt.plot()
 
-
-# Create slider widget for the weights parameter
-weight_slider = st.sidebar.slider(min=0, max=0.99, step=0.1, value=0.5, description='Weight:')
-# Define function to update the plot when the slider value changes
-def update_plot(change):
-    weight = change.new
-    generate_dataset_and_roc_auc(weight)
-# Register the update_plot function to be called when the slider value changes
-weight_slider.observe(update_plot, 'value')
+# plt.box(False)
+# plt.show()
 st.pyplot(fig)
